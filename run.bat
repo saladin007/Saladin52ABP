8c4de3e69b50d4cd236fb8c42bacc675e4dd8f02
e <116293678@qq.com>
2019-01-14 17:19:16 +0800

    Signed-off-by: e <116293678@qq.com>

---
+++ run.bat
@@ -0,0 +1,6 @@
+echo "# Saladin52ABP" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/saladin007/Saladin52ABP.git
+git push -u origin master
